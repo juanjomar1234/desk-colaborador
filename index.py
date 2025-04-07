353ec40f5c2dffe7b018c7bccdbc7fcef85b37ca
juanjomar1234 <juanjomar.gmx@gmail.com>
2025-04-07 06:34:43 +0200

    Add shebang and CGI headers to Python files, update permissions

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 from frontend_service.app import app
 
 if __name__ == "__main__":
+    print("Content-Type: text/html\n")  # Necesario para CGI
     app.run(host='0.0.0.0', port=5000) 
